d6a7111e2cfbe237025b4125aec56fde6a19b2e0
ShutUpPaulo <markes.calado@gmail.com>
2017-07-04 12:09:49 -0300

    Merging refactoring_place with master.

---
+++ gestorpsi/place/tests.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2008 GestorPsi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
+
+from gestorpsi.address.models import Address
+from gestorpsi.phone.models import Phone, PhoneType
+from gestorpsi.place.models import PlaceType, Place
+from gestorpsi.address.models import City, State, Country, AddressType
+import unittest
+
+from django.test import TestCase, RequestFactory
+from django.test import Client as Cl
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from gestorpsi.util.test_utils import user_stub, setup_required_data, place_stub
+
+class PlaceTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        # OBS: Using cl instead of client to evade conflicts with Client class
+        self.client = Cl()
+        setup_required_data()
+
+    def test_show_list_of_places(self):
+        self.client.post(reverse('registration-register'), user_stub())
+        res = self.client.login(username=user_stub()["username"], password=user_stub()["password1"])
+        user = User.objects.get(username=user_stub()["username"])
+        user.is_superuser = True
+        user.save()
+        response= self.client.get('/place/')
+        self.assertEquals(200,response.status_code)
+        self.assertTemplateUsed(response, 'place/place_list.html')
+
+    def test_not_show_list_for_unlogged_user(self):
+        self.client.logout() # guarantee that the given user is signed out
+        response= self.client.get('/place/')
+        self.assertEquals(302,response.status_code)
+
+    def test_show_add_place(self):
+        self.client.post(reverse('registration-register'), user_stub())
+        res = self.client.login(username=user_stub()["username"], password=user_stub()["password1"])
+        user = User.objects.get(username=user_stub()["username"])
+        user.is_superuser = True
+        user.save()
+
+        old_place_count = Place.objects.count()
+        response= self.client.post('/place/save/', place_stub())
+
+        new_place = Place.objects.all()[0]
+        new_place_response =  self.client.get('/place/'+str(new_place.id)+'/')
+        self.assertEquals(200,new_place_response.status_code)
+        self.assertEquals(Place.objects.count(), old_place_count+1)
+
+    def test_not_show_form_for_unlogged_user(self):
+        self.client.logout() # guarantee that the given user is signed out
+        response= self.client.get('/place/add/')
+        self.assertEquals(302,response.status_code)
+
+    def test_show_deactive_places(self):
+        self.client.post(reverse('registration-register'), user_stub())
+        res = self.client.login(username=user_stub()["username"], password=user_stub()["password1"])
+        user = User.objects.get(username=user_stub()["username"])
+        user.is_superuser = True
+        user.save()
+        response= self.client.get('/place/deactive/')
+        self.assertEquals(200,response.status_code)
+        self.assertTemplateUsed(response, 'place/place_list.html')
+
+    def test_not_show_deactive_places_for_unlogged_user(self):
+        self.client.logout() # guarantee that the given user is signed out
+        response= self.client.get('/place/deactive/')
